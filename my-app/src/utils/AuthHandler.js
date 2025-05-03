ac58c2fcd7485372767a4351f2e06bafe66767c1
unknown <ss2001shershah@gmail.com>
2025-05-03 07:36:46 +0530

    working on login section

---
+++ my-app/src/utils/AuthHandler.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import Config from "./config";
+import { reactLocalStorage } from "reactjs-localstorage";
+
+class AuthHandler{
+    static login(username,password,callback){
+     axios.
+     post(Config.loginUrl,{username:username,password:password})
+     .then(function(response){
+        if(response.status==200){
+             reactLocalStorage.set("token",response.data.access)
+             reactLocalStorage.set("token",response.data.refresh)
+        }
+     })
+     .catch(function(error){
+        console.log(error.response);
+     });
+    }
+}
+
+export default AuthHandler;
